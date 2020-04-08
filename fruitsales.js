8aa7ae41a175e6f644231bd54fe07dc2d56064e7
Michael Spremulli <michaels.wpi@gmail.com>
2020-04-07 21:12:16 -0400

    made a loop

---
+++ fruitsales.js
@@ -0,0 +1,13 @@
+//fruit stand
+
+let storeName="Felicia's Fruit Stand";
+let slogan="Buy Felicia";
+let fruitNames=['Apple','Orange','Pear'];
+let fruitCosts=[1.5,1,2];
+let fruitSales=[125,230,59];
+
+console.log(`Come ${slogan} at ${storeName}!`)
+
+for(let i=0;i<fruitCosts.length;i++){
+    console.log(`${fruitNames[i]}s cost $${fruitCosts[i].toFixed(2)} each. We sold ${fruitSales[i]} today.`);
+}
