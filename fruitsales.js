c99076d2709c42fef8d2a3037279880d6f333fc2
Michael Spremulli <michaels.wpi@gmail.com>
2020-04-07 21:24:49 -0400

    added comments

--- fruitsales.js
+++ fruitsales.js
@@ -11,3 +11,4 @@ console.log(`Come ${slogan} at ${storeName}!`)
 for(let i=0;i<fruitCosts.length;i++){
     console.log(`${fruitNames[i]}s cost $${fruitCosts[i].toFixed(2)} each. We sold ${fruitSales[i]} today.`);
 }
+//add some new loops here
